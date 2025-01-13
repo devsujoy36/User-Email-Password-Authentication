f0ccf80b7cfc5b659ba9dcada0b1dba181cdd924
unknown <sujoydaspc2023@gmail.com>
2025-01-13 23:23:14 +0600

    adding change in login register

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -1,28 +1,27 @@
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../Firebase/Firebase.config";
 import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { BiSolidHide } from "react-icons/bi";
 import { BiShow } from "react-icons/bi";
-
 const LogIn = () => {
     const btnStyle = "bg-emerald-500 py-2 px-4 rounded-lg hover:bg-transparent border-2 border-transparent hover:border-black  font-semibold active:scale-95 cursor-pointer transition-all"
 
     const [user, setUser] = useState()
+    console.log(user);
     const [registerError, setRegisterError] = useState('')
     const [success, setSuccess] = useState('')
-    // const [showPassword, setShowPassword] = useState('')
     const [showHidePass, setShowHidePass] = useState(true)
 
     const handleShowHidePass = () => {
-      if (showHidePass === true) {
-        setShowHidePass(!showHidePass)
-      }
-      else {
-        setShowHidePass(true)
-      }
+        if (showHidePass === true) {
+            setShowHidePass(!showHidePass)
+        }
+        else {
+            setShowHidePass(true)
+        }
     }
-    console.log(user);
 
     const handleRegister = (e) => {
         e.preventDefault()
@@ -51,27 +50,45 @@ const LogIn = () => {
             })
     }
 
+
+    const googleProvider = new GoogleAuthProvider()
+    const handleGoogleLogin = () => {
+        setRegisterError('')
+        setSuccess('')
+        signInWithPopup(auth, googleProvider)
+            .then(res => {
+                setUser(res.user)
+                setSuccess('User Created Successfully')
+            })
+            .Catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div className="max-w-screen-2xl lg:my-20 pb-10 lg:mx-auto mx-10">
             <Helmet>
                 <title>UEPA | LogIn </title>
             </Helmet>
-            <div>
-                <form onSubmit={handleRegister} className="flex flex-col gap-5 lg:w-4/12 md:w-6/12 mx-auto border p-10 rounded-md shadow-lg my-10">
+            <div className="rounded-md shadow-lg my-10 border p-10">
+                <form onSubmit={handleRegister} className="flex flex-col gap-5 lg:w-4/12 md:w-6/12 mx-auto   ">
                     <h1 className="text-4xl font-bold text-center text-emerald-600">Log In</h1>
                     <input type="email" name="email" placeholder="Email" className="border rounded-md px-4 py-3" required />
                     <div className='flex justify-between items-center border rounded-md relative'>
-            <input type={showHidePass ? "password" : "text"} name='password' placeholder='Password' className='border  w-full rounded-md px-4 py-3' required />
-            <div onClick={handleShowHidePass} className='absolute right-2 text-xl'>{showHidePass ? <BiSolidHide /> : <BiShow />}</div>
-          </div><input type="submit" value={"Register"} className={btnStyle} />
-                    <div className="mt-6 flex flex-col gap-3 justify-between">
-                        <button type="submit" className={btnStyle} >Continue with Google </button>
-                        <button type="submit" className={btnStyle} >Continue with Github </button>
-                        <button type="submit" className={btnStyle} >Continue with Facebook</button>
+                        <input type={showHidePass ? "password" : "text"} name='password' placeholder='Password' className='border  w-full rounded-md px-4 py-3' required />
+                        <div onClick={handleShowHidePass} className='absolute right-2 text-xl'>{showHidePass ? <BiSolidHide /> : <BiShow />}</div>
+                    </div>
+                    <input type="submit" value={"Log In"} className={btnStyle} />
+                </form>
+                <div>
+                <div className="mt-6 flex flex-col gap-3 justify-between">
+                        <button onClick={handleGoogleLogin} className={btnStyle} >Continue with Google </button>
+                        <button type="" className={btnStyle} >Continue with Github </button>
+                        <button type="" className={btnStyle} >Continue with Facebook</button>
                     </div>
                     {registerError && <p className="text-xs text-red-500">{registerError}</p>}
                     {success && <p className="text-xs text-emerald-500">{success}</p>}
-                </form>
+                </div>
             </div>
         </div>
     )
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,5 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import auth from '../../Firebase/Firebase.config'
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
@@ -16,14 +17,7 @@ const Register = () => {
 
   const [showHidePass, setShowHidePass] = useState(true)
 
-  const handleShowHidePass = () => {
-    if (showHidePass === true) {
-      setShowHidePass(!showHidePass)
-    }
-    else {
-      setShowHidePass(true)
-    }
-  }
+ 
 
   const handleRegister = e => {
     e.preventDefault()
@@ -60,15 +54,29 @@ const Register = () => {
       })
   }
 
+  const googleProvider = new GoogleAuthProvider()
+  const handleGoogleLogin = () => {
+    setRegisterError('')
+    setSuccess('')
+    signInWithPopup(auth, googleProvider)
+      .then(res => {
+        setUser(res.user)
+        setSuccess('User Created Successfully')
+      })
+      .Catch(error => {
+        console.log(error);
+      })
+  }
+
   return (
     <div className='max-w-screen-2xl lg:my-20 lg:mx-auto mx-10'>
       <Helmet>
         <title>UEPA | Register </title>
       </Helmet>
-      <div>
+      <div className='border p-10 rounded-md shadow-lg my-10'>
         <form
           onSubmit={handleRegister}
-          className='flex flex-col gap-5 lg:w-4/12 md:w-6/12 mx-auto border p-10 rounded-md shadow-lg my-10'
+          className='flex flex-col gap-5 lg:w-4/12 md:w-6/12 mx-auto '
         >
           <h1 className='text-4xl font-bold text-center text-emerald-600'>
             Register
@@ -78,24 +86,20 @@ const Register = () => {
           <input type='number' name='phone' placeholder='Phone' className='border rounded-md px-4 py-3' required />
           <div className='flex justify-between items-center border rounded-md relative'>
             <input type={showHidePass ? "password" : "text"} name='password' placeholder='Password' className='border  w-full rounded-md px-4 py-3' required />
-            <div onClick={handleShowHidePass} className='absolute right-2 text-xl'>{showHidePass ? <BiSolidHide /> : <BiShow />}</div>
+            <div onClick={()=>setShowHidePass(!showHidePass)} className='absolute right-2 text-xl'>
+              {showHidePass ? <BiSolidHide /> : <BiShow />}
+              </div>
           </div>
 
           <input type='submit' value={'Register'} className={btnStyle} />
-          <div className='mt-6 flex flex-col gap-3 justify-between'>
-            <button type='submit' className={btnStyle}>
-              Continue with Google{' '}
-            </button>
-            <button type='submit' className={btnStyle}>
-              Continue with Github{' '}
-            </button>
-            <button type='submit' className={btnStyle}>
-              Continue with Facebook
-            </button>
-          </div>
-          {registerError && <p className='text-xs text-red-500'>{registerError}</p>}
-          {success && <p className='text-xs text-emerald-500'>{success}</p>}
         </form>
+        <div className="mt-6 flex flex-col gap-3 justify-between">
+          <button onClick={handleGoogleLogin} className={btnStyle} >Continue with Google </button>
+          <button type="" className={btnStyle} >Continue with Github </button>
+          <button type="" className={btnStyle} >Continue with Facebook</button>
+        </div>
+        {registerError && <p className="text-xs text-red-500">{registerError}</p>}
+        {success && <p className="text-xs text-emerald-500">{success}</p>}
       </div>
     </div>
   )
