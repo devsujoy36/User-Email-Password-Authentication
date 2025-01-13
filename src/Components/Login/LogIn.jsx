f511c3ae1e85f8f9cba9b4d348aee232f4dd6ca7
unknown <sujoydaspc2023@gmail.com>
2025-01-14 01:58:45 +0600

    remove padding in login

--- src/Components/Login/LogIn.jsx
+++ src/Components/Login/LogIn.jsx
@@ -6,15 +6,20 @@ import { Helmet } from "react-helmet-async";
 import { BiSolidHide } from "react-icons/bi";
 import { BiShow } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 const LogIn = () => {
-    const btnStyle = "bg-emerald-500 py-2 px-4 rounded-lg hover:bg-transparent border-2 border-transparent hover:border-black  font-semibold active:scale-95 cursor-pointer transition-all"
 
+    
+    const btnStyle = "bg-emerald-500 py-1 rounded-lg hover:bg-transparent border-2 border-transparent hover:border-black font-semibold active:scale-95 cursor-pointer transition-all"
+    
     const [user, setUser] = useState()
     console.log(user);
-    const [registerError, setRegisterError] = useState('')
-    const [success, setSuccess] = useState('')
-    const [showHidePass, setShowHidePass] = useState(true)
 
+    const [loginError, setLogInError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const [showHidePass, setShowHidePass] = useState(true)
+    
     const handleShowHidePass = () => {
         if (showHidePass === true) {
             setShowHidePass(!showHidePass)
@@ -23,60 +28,65 @@ const LogIn = () => {
             setShowHidePass(true)
         }
     }
-
+    
     const handleRegister = (e) => {
         e.preventDefault()
-
+        
+        
         const email = e.target.email.value;
         const password = e.target.password.value;
-
+        
         console.log(email, " ", password);
-
+        
         if (password.length < 6) {
-            setRegisterError('Password should be at least 6 characters or longer');
+            setLogInError('Password should be at least 6 characters or longer');
+            notify(`${loginError}`)
             return;
         }
-
-        setRegisterError('')
+        
+        setLogInError('')
         setSuccess('')
-
+        
         createUserWithEmailAndPassword(auth, email, password)
-            .then(res => {
-                setUser(res.user)
-                setSuccess('User Created Successfully')
-            })
-            .catch(error => {
-                setRegisterError(error.message);
-                console.log(error.message);
-            })
+        .then(res => {
+            setUser(res.user)
+            setSuccess('User Created Successfully')
+            
+        })
+        .catch(error => {
+            setLogInError(error.message);
+            console.log(error.message);
+        })
     }
-
-
+    
+    
     const googleProvider = new GoogleAuthProvider()
     const handleGoogleLogin = () => {
-        setRegisterError('')
+        setLogInError('')
         setSuccess('')
         signInWithPopup(auth, googleProvider)
-            .then(res => {
-                setUser(res.user)
-                setSuccess('User Created Successfully')
-            })
-            .Catch(error => {
-                console.log(error);
-            })
+        .then(res => {
+            setUser(res.user)
+            setSuccess('User Created Successfully')
+        })
+        .Catch(error => {
+            console.log(error);
+        })
     }
-
+    const notify = (text) => toast(text);
+    
     return (
         <div className="max-w-screen-2xl lg:my-20 pb-10 lg:mx-auto mx-10">
+            <ToastContainer />
             <Helmet>
                 <title>UEPA | LogIn </title>
             </Helmet>
-            <div className="rounded-md shadow-lg my-10 border p-10 lg:w-4/12 md:w-6/12 mx-auto">
+            <div className="rounded-md shadow-lg my-10 border p-10 lg:w-3/12 md:w-6/12 mx-auto">
                 <form onSubmit={handleRegister} className="flex flex-col gap-5 ">
                     <h1 className="text-4xl font-bold text-center text-emerald-600">Log In</h1>
-                    <input type="email" name="email" placeholder="Email" className="border rounded-md px-4 py-3" required />
+                    <input type="email" name="email" placeholder="Email" className="border rounded-md px-4 py-2" required />
                     <div className='flex justify-between items-center border rounded-md relative'>
-                        <input type={showHidePass ? "password" : "text"} name='password' placeholder='Password' className='border  w-full rounded-md px-4 py-3' required />
+                        <input type={showHidePass ? "password" : "text"} name='password' placeholder='Password' className='border  w-full rounded-md px-4 py-2' required />
                         <div onClick={handleShowHidePass} className='absolute right-2 text-xl'>{showHidePass ? <BiSolidHide /> : <BiShow />}</div>
                     </div>
                     <div className='flex text-xs gap-1 justify-between items-center'>
@@ -91,8 +101,10 @@ const LogIn = () => {
                         <button type="" className={btnStyle} >Continue with Github </button>
                         <button type="" className={btnStyle} >Continue with Facebook</button>
                     </div>
-                    {registerError && <p className="text-xs text-red-500">{registerError}</p>}
-                    {success && <p className="text-xs text-emerald-500">{success}</p>}
+                    <div className="mt-1">
+                        {loginError && <p className="text-xs text-red-500">{loginError}</p>}
+                        {success && <p className="text-xs text-emerald-500">{success}</p>}
+                    </div>
                 </div>
             </div>
         </div>
