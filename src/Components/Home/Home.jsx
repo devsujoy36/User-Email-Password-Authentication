bb2e21ea38e0ff68229b2bc2d8c758a8e21f9061
unknown <sujoydaspc2023@gmail.com>
2025-01-15 23:13:21 +0600

    upload change

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,10 +15,8 @@ const Home = () => {
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-          </p>
+          <p className="mb-5"> Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+            quasi. In deleniti eaque aut repudiandae et a id nisi. </p>
           <Link to={"/login"} className="btn btn-primary">Get Started</Link>
         </div>
       </div>
